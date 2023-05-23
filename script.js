651a018243372e46f89ae2de98b5ca9d371d7791
Pontes <hpo.pontes@gmail.com>
2023-05-23 16:10:35 -0300

    adicionando span e funcao dados na tela

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 //pegando uma API para pegar as informações externar
 const key = "a0dcd14f7f427f1ae1bd5ff8f888231e"
 async function buscaCidade(cidade){
-    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}`).then( resposta => resposta.json())
-    console.log(dados)
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then( resposta => resposta.json())
+    //console.log(dados)
+    dadosTela(dados)
+}
+function dadosTela(dados){
+    const title = document.getElementById('cidade_title')
+    title.innerHTML = dados.name
+    title.classList.add('yello')
+    document.querySelector('.temp').innerHTML = dados.
 }
 function pegaInput() {
    const cidade = document.getElementById('input').value
