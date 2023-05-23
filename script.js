848ee9787c30121cc9d058a28bc24669fddb36bc
Pontes <hpo.pontes@gmail.com>
2023-05-23 16:00:48 -0300

    testando no console os dados

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //pegando uma API para pegar as informações externar
-const key = 'a0dcd14f7f427f1ae1bd5ff8f888231e'
+const key = "a0dcd14f7f427f1ae1bd5ff8f888231e"
 async function buscaCidade(cidade){
-    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}`)
-    
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}`).then( resposta => resposta.json())
+    console.log(dados)
 }
 function pegaInput() {
    const cidade = document.getElementById('input').value
