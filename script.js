7b3e59cbc12e1452619227a8e59df18e5348ae5c
Pontes <hpo.pontes@gmail.com>
2023-05-23 16:19:43 -0300

    commentando codigo e pegando a umidade

--- script.js
+++ script.js
@@ -2,14 +2,16 @@
 const key = "a0dcd14f7f427f1ae1bd5ff8f888231e"
 async function buscaCidade(cidade){
     const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then( resposta => resposta.json())
-    //console.log(dados)
+    console.log(dados)
     dadosTela(dados)
 }
 function dadosTela(dados){
     const title = document.getElementById('cidade_title')
-    title.innerHTML = dados.name
-    title.classList.add('yello')
-    document.querySelector('.temp').innerHTML = dados.main.temp + "°C"
+    title.innerHTML = dados.name // pega o nome
+    title.classList.add('verde-titulo') // adiciona o verde no titulo
+    document.querySelector('.temp').innerHTML = dados.main.temp + "°C" // pega a temperatura 
+    document.querySelector('.text-previsao').innerHTML = dados.weather[0].description // descrição
+    document.querySelector('#umidade').innerHTML = dados.main.humidity + "%" // pega a umidade 
 }
 function pegaInput() {
    const cidade = document.getElementById('input').value
