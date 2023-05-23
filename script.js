3bf476164137487fdd03bc7ca92f8e9bc6dd1d5f
Pontes <hpo.pontes@gmail.com>
2023-05-23 15:51:55 -0300

    adicionando o https da API e utilizando o async e await fetch

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 //pegando uma API para pegar as informações externar
 const key = 'a0dcd14f7f427f1ae1bd5ff8f888231e'
+async function buscaCidade(cidade){
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}`)
+    
+}
+function pegaInput() {
+   const cidade = document.getElementById('input').value
+   buscaCidade(cidade)
+   //console.log(input)
+}
