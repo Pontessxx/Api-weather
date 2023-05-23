986c36c04a16cf985e7cdd2dc4b527c65dbe2c7a
Pontes <hpo.pontes@gmail.com>
2023-05-23 16:27:58 -0300

    icone interativo

--- script.js
+++ script.js
@@ -9,9 +9,11 @@ function dadosTela(dados){
     const title = document.getElementById('cidade_title')
     title.innerHTML = dados.name // pega o nome
     title.classList.add('verde-titulo') // adiciona o verde no titulo
-    document.querySelector('.temp').innerHTML = dados.main.temp + "°C" // pega a temperatura 
+    document.querySelector('.temp').innerHTML = Math.floor(dados.main.temp) + "°C" // pega a temperatura 
     document.querySelector('.text-previsao').innerHTML = dados.weather[0].description // descrição
     document.querySelector('#umidade').innerHTML = dados.main.humidity + "%" // pega a umidade 
+    document.querySelector('.img-previsao').src =`https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+
 }
 function pegaInput() {
    const cidade = document.getElementById('input').value
